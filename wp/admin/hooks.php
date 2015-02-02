a41c2dbb5debb1066e021ccac71c168b4d0bbba6
PurpleDrain <kba_93@live.se>
2015-02-02 10:57:25 +0100

    fix innovatordm/bygdeband#215

--- wp/admin/hooks.php
+++ wp/admin/hooks.php
@@ -4,6 +4,7 @@ namespace elasticsearch;
 class Hooks{
 	function __construct(){
 		add_action( 'save_post', array( &$this, 'save_post' ) );
+		add_action( 'edit_attachment', array( &$this, 'save_post' ) );
 		add_action( 'delete_post', array( &$this, 'delete_post' ) );
 		add_action( 'trash_post', array( &$this, 'delete_post' ) );
 	}
@@ -23,7 +24,7 @@ class Hooks{
 			Indexer::delete($post);
 		}
 
-		if ($post->post_status == 'publish'){
+		if ($post->post_status == 'publish' || $post->post_status == 'inherit'){
 			Indexer::addOrUpdate($post);
 		}
 	}
