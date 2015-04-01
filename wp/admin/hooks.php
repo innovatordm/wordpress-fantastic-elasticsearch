ead760083ea2fa5b3b99624614c84fff3c0ce133
Jonas Stensved <jonas.stensved@stonesmart.se>
2015-04-01 22:47:16 +0200

    lower prio so meta manipulation etc can occur before save

--- wp/admin/hooks.php
+++ wp/admin/hooks.php
@@ -3,10 +3,10 @@ namespace elasticsearch;
 
 class Hooks{
 	function __construct(){
-		add_action( 'save_post', array( &$this, 'save_post' ) );
-		add_action( 'edit_attachment', array( &$this, 'save_post' ) );
-		add_action( 'delete_post', array( &$this, 'delete_post' ) );
-		add_action( 'trash_post', array( &$this, 'delete_post' ) );
+		add_action( 'save_post', array( &$this, 'save_post' ) , 999);
+		add_action( 'edit_attachment', array( &$this, 'save_post' ) , 999);
+		add_action( 'delete_post', array( &$this, 'delete_post' ) , 999);
+		add_action( 'trash_post', array( &$this, 'delete_post' ) , 999);
 	}
 	
 	function save_post( $post_id ) {
